189799f84f3c178b6abf612183669166cf5e4d06
Umer qureshi <Umar Qureshi>
2024-02-12 18:28:37 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+functions , objects ,  arrays and loops
+
+                HIGH ORDER FUNCTIONS 
+
+*/
+// hoisting
+
+
+// function declaration
+function test(){
+    console.log("hello");
+}
+
+// anonymous function , function expression 
+
+const test2 = function(){
+    return "test"
+}
+
+console.log(test2("umer"))
